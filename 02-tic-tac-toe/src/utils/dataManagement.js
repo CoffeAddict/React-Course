f79e04d2e28683d049be5b591ffb9aadaa4d8533
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-08-04 03:05:18 -0300

    added winner storage handlers

--- 02-tic-tac-toe/src/utils/dataManagement.js
+++ 02-tic-tac-toe/src/utils/dataManagement.js
@@ -16,9 +16,19 @@ function getTurnFromStorage () {
   return JSON.parse(localData) || null
 }
 
+function saveWinnerOnStorage (winner) {
+  localStorage.setItem('ttt-winner', JSON.stringify(winner))
+}
+
+function getWinnerFromStorage () {
+  const localData = localStorage.getItem('ttt-winner')
+  return JSON.parse(localData) || null
+}
+
 function clearStorage () {
   localStorage.removeItem('ttt-board')
   localStorage.removeItem('ttt-turn')
+  localStorage.removeItem('ttt-winner')
 }
 
-export { saveBoardOnStorage, getBoardFromStorage, saveTurnOnStorage, getTurnFromStorage, clearStorage }
+export { saveBoardOnStorage, getBoardFromStorage, saveTurnOnStorage, getTurnFromStorage, saveWinnerOnStorage, getWinnerFromStorage, clearStorage }
